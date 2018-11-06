449fab67632cd0ede3c613363fb3e65cbddf220f
Mateus Gonçalo <mateusgon57@gmail.com>
2018-11-06 11:37:13 -0200

    Adicionando o DesativarComboAction e o DesativarItemAction

--- src/java/action/DesativarComboAction.java
+++ src/java/action/DesativarComboAction.java
@@ -4,7 +4,7 @@ import controller.Action;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class EditarComboAction implements Action{
+public class DesativarComboAction implements Action{
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
--- src/java/action/DesativarItemAction.java
+++ src/java/action/DesativarItemAction.java
@@ -6,13 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import persistence.ProdutoDAO;
 
-public class ExcluirItemAction implements Action {
+public class DesativarItemAction implements Action {
 
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
         Integer idRestaurante = Integer.parseInt(request.getParameter("id2"));
         Integer idProduto = Integer.parseInt(request.getParameter("id"));
-        ProdutoDAO.getInstance().deleteProduto(idProduto);
+       // ProdutoDAO.getInstance().deleteProduto(idProduto);
         request.setAttribute("idRest", idRestaurante);
         request.setAttribute("produtos", ProdutoDAO.getInstance().listAllFromRestaurante(idRestaurante));
         RequestDispatcher dispatcher = request.getRequestDispatcher("acesso-restrito-superusuario-listar-produtos.jsp");
